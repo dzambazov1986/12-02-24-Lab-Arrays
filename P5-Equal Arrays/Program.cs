33524193f2da2e4004bf2dbd4b7b595a44d6791b
Dimityr <dzambazov1986@gmail.com>
2024-02-15 23:56:44 +0200

    Add project files.

---
+++ P5-Equal Arrays/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+int[] numbers1  = Console.ReadLine()
+                   .Split()
+                   .Select(int.Parse)
+                   .ToArray();
+
+int[] numbers2 = Console.ReadLine()
+                   .Split()
+                   .Select(int.Parse)
+                   .ToArray();
+
+bool isIdentical = true;
+
+for (int i = 0; i < numbers1.Length; i++)
+{
+    if (numbers1[i] != numbers2[i])
+    {
+        isIdentical = false; 
+        break;
+    }
+} 
+
+if (isIdentical)
+{
+    Console.WriteLine("Arrays are identical.");
+}
+else
+{
+    Console.WriteLine("Arrays are not identical.");
+}
---
+++ P6-Common Elements/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.Design;
+using System.Xml.Schema;
+
+string currency = Console.ReadLine();
+int sum = int.Parse(Console.ReadLine());
+double itemPrice = 1000; // You can set the item price here
+
+if (currency == "EUR")
+{
+    double total = (sum * 1.955830);
+    Console.WriteLine($"Total money for exchange is {total:F2} BGN");
+
+    if (total < itemPrice)
+    {
+        double remaining = itemPrice - total;
+        Console.WriteLine($"You need to pay {remaining:F2} BGN more for the item.");
+    }
+}
+else if (currency == "BGN")
+{
+    double total = sum / 1.955830;
+    Console.WriteLine($"Total money for exchange is {total:F2} EUR");
+}
+
+
+
+
+
+
+
+
